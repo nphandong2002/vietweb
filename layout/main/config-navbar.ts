24094f773dd6b0e8de6e19119e4ad2babebb5c45
conquy2002 <72489231+conquy2002@users.noreply.github.com>
2024-02-28 18:44:07 +0700

    base

---
+++ layout/main/config-navbar.ts
@@ -0,0 +1,2 @@
+export const heightNav = '50px';
+export const widthNav = '100px';
--- layout/main/main-layout.tsx
+++ layout/main/main-layout.tsx
@@ -1,14 +1,19 @@
-import { Box } from "@mui/material";
-import Navbar from "./navbar";
+import { Box } from '@mui/material';
+import Navbar from './navbar';
+import { heightNav, widthNav } from './config-navbar';
 
 function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <nav className="bottom-0 fixed z-50 w-screen ">
-        <Box>
-          <div className="flex flex-row md:flex-col text-[30px] justify-center border border-r">
-            <Navbar />
-          </div>
+      <nav className="bottom-0 fixed z-50">
+        <Box
+          className="flex flex-row md:flex-col w-screen text-[30px] justify-center  border border-r"
+          sx={{
+            height: { sx: heightNav, md: '100vh' },
+            width: { md: widthNav },
+          }}
+        >
+          <Navbar />
         </Box>
       </nav>
       <div className="mx-[var(--header-size)]">
