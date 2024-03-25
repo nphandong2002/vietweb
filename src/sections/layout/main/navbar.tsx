6bff4161058f82d51018c847565e8f634702fb60
nphandong2002 <103531584+nphandong2002@users.noreply.github.com>
2024-03-25 22:44:47 +0700

    base

--- src/sections/layout/main/navbar.tsx
+++ src/sections/layout/main/navbar.tsx
@@ -1,10 +1,10 @@
-import { H_NAV } from 'src/config';
-import { cn } from 'src/lib/utils';
+import { H_NAV, M_W_NAV, W_NAV } from "src/config";
+import { cn } from "src/lib/utils";
 
 function NavBar() {
   return (
     <nav className="fixed z-[90] bottom-0">
-      <div className={'md:h-screen w-screen' + cn(`h-[${H_NAV}px]`)}>
+      <div className={"md:h-screen w-screen " + cn(`h-[${H_NAV}px] sm:w-[${M_W_NAV}px] md:w-[${W_NAV}px]`)}>
         <div className="flex flex-row md:flex-col"> navbar</div>
       </div>
     </nav>
